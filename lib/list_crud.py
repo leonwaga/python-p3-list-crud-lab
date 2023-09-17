557ef83ba153c293f3a7215adf2566de91e97626
leon <leon.waga@student.moringaschool.comgit config --global user.name leongit config --global user.name leon>
2023-09-18 01:51:31 +0300

    completed assignment

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,32 @@
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    list = [23, True, 8.8, "audi"]
+    return list
 
-def add_element_to_end_of_list(l, element):
-    return None
+def add_element_to_end_of_list(list,element):
+    list.append(element)
+    return list
 
-def add_element_to_start_of_list(l, element):
-    return None
+def add_element_to_start_of_list(list, element):
+    list.insert(0, element)
+    return list 
 
-def remove_element_from_end_of_list(l):
-    return None
+def remove_element_from_end_of_list(list):
+    list.pop()
+    return list
 
-def remove_element_from_start_of_list(l):
-    return None
+def remove_element_from_start_of_list(list):
+    list.delete()
+    return list
 
-def retrieve_first_element_from_list(l):
-    return None
+def retrieve_first_element_from_list(list):
+    return list.index(list[0])
 
-def retrieve_element_from_index(l, index):
-    return None
+def retrieve_element_from_index(list, index):
+    return list[index]
 
-def retrieve_last_element_from_list(l):
-    return None
+def retrieve_last_element_from_list(list):
+    return list[-1]
+
